f8451b02ebc50be02fdc8fb4a7fd65f023425166
oozoemenam <oozoemenam@gmail.com>
2023-09-03 14:55:10 +0200

    first commit

---
+++ src/main/java/com/example/real_time_com/RealTimeComApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_time_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/real_time_com/RealTimeComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_time_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
